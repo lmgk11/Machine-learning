3c7b4b505d58a7674200cb25eb76c8b4d8a0a487
lmgk11 <lulle.gillbergkling@gmail.com>
2017-09-21 18:36:10 +0200

    Source changes

--- Perceptron.java
+++ Perceptron.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class Perceptron {
 	private float[] m_weights	= new float[3]; 	//two for the inputs and one for the bias
-	private double learningrate	= 0.01;             //how fast the weights change
-	
+	private double learningrate	= 0.000001;             //how fast the weights change wich results in how fast it learns and how accurate it can get
+													//if its lower it will take longer time but it will be more accurate
 	Random rand = new Random();
 	
 	public Perceptron() {
 		//Setting all the weights to random numbers
-		System.out.println("Starting Weights : ");
+		System.out.print("Starting Weights : ");
 		for (int i = 0; i < m_weights.length; i++) {
 			m_weights[i] = (float) (rand.nextGaussian() * 1.0f);
 			System.out.print(m_weights[i] + " : ");
@@ -18,13 +18,12 @@ public class Perceptron {
 		System.out.println("\n");
 	}
 	
-	public float feedForward(float[] inputs) {
+	public int feedForward(float[] inputs) {
 		float sum = 0;
 		for (int i = 0; i < m_weights.length; i++) {
 			sum += m_weights[i] * inputs[i];
 		}
-		//return activate(sum); //this will be used if you want the perceptron to answer yes or no questions
-		return sum;             //this is just by default
+		return activate(sum); //this will be used if you want the perceptron to answer yes or no questions
 	}
 	
 	public void train(float[] inputs, int target) {
--- Source.java
+++ Source.java
@@ -8,24 +8,119 @@ Author: Lucas Gillberg Kling
 
 ************************************************************************/
 
-import Perceptron.*;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.util.Random;
 
-public class Source {
+import javax.swing.JFrame;
+
+import Perceptron.Perceptron;
+
+public class Source extends Canvas implements Runnable {
+	point[] points;
+	
+	int length = points.length;
+	int right  = 0;
+	
 	private static final long serialVersionUID = 1L;	//Not needed
 	
 	static Perceptron ptron = new Perceptron();
 	
+	public static final int HEIGHT = 600;
+	public static final int WIDTH = 600;
+	
+	private Thread thread;
+	private BufferStrategy bs = null;
+	private Graphics graphics = null;
+	
+	private boolean running = false;
+	
 	public static void main(String[] args) {
-		float[] inputs = {
-				0,5,
-				-1,5,
-				1 //bias
-		};
+		points = new point[Integer.Parse(args[0])];
+		new Source();
+	}
+	
+	private Source() {
+		JFrame frame = new JFrame("Machine Learning");
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.add(this);
 		
-		System.out.println(ptron.feedForward(inputs));
-		for (int i = 0; i < 100; i++) {
-			ptron.train(inputs, 1);
-			System.out.println(ptron.feedForward(inputs));
+		for (int i = 0; i < points.length; i++) {
+			points[i] = new point();
+		}
+		
+		thread = new Thread(this);
+		
+		frame.setVisible(true);
+	}
+	
+	public void paint(Graphics g) {
+		if(bs == null) {
+			createBufferStrategy(2);
+			bs = getBufferStrategy();
+			graphics = bs.getDrawGraphics();
+			
+			thread.start();
+			running = true;
+		}
+	}
+
+	public void update() {
+		
+	}
+	
+	public void render() {
+		right = 0;
+		graphics.clearRect(0, 0, WIDTH, HEIGHT);
+		graphics.setColor(Color.black);
+		for (int i = 0; i < points.length; i++) {
+			ptron.train(points[i].inputs, points[i].label);
+			int guess = ptron.feedForward(points[i].inputs);
+			
+			if (guess == points[i].label) {
+				right++;
+				graphics.setColor(Color.GREEN);
+				
+				if(points[i].label == 1) {
+					graphics.drawString("Down", points[i].x, points[i].y);
+				} else {
+					graphics.drawString("Up", points[i].x, points[i].y);
+				}
+			} else {
+				graphics.setColor(Color.RED);
+				
+				if(points[i].label == 1) {
+					graphics.drawString("Up", points[i].x, points[i].y);
+				} else {
+					graphics.drawString("Down", points[i].x, points[i].y);
+				}
+			}
+			
+			points[i].show(graphics);
+			graphics.setColor(Color.BLACK);
+			graphics.drawLine(0, HEIGHT/2 - 10, WIDTH, HEIGHT/2 - 10);
+		}
+	}
+	
+	@Override
+	public void run() {
+		while(running) {
+			update();
+			render();
+			
+			bs.show();
+			
+			Thread.currentThread();
+			try {
+				Thread.sleep(1000);
+			} catch(InterruptedException e) {
+				
+			}
 		}
 	}
 }
